604bf0e1883db242403178417e61e91267eec9ad
Jon Bauman <jongithub@shumi.fake>
2015-10-07 17:24:39 -0400

    Finish set 1 challenge 1, clean up comments, make file run rspec if interpreted directly

--- hex_to_base64.rb
+++ hex_to_base64.rb
@@ -9,6 +9,14 @@
 
 # Cryptopals Rule
 # Always operate on raw bytes, never on encoded strings. Only use hex and base64 for pretty-printing.
+
+
+if __FILE__ == $0
+  puts `rspec -f documentation #{__FILE__}`
+else
+  require "rspec"
+end
+
 describe :hex_to_base64 do
   HEX_STRING = '49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'
   BASE64_STRING = 'SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t'
@@ -22,28 +30,38 @@ describe :hex_to_base64 do
   end
 end
 
-# hex digits encode 4 bits (2**4 == 16)
-# base64 digits encode 6 bits (2**6 == 64)
-# So, hex 492 == b64 SS
+# I've never actually implemented base64, but rather than look it up, let's
+# have some fun and see if we can figure it out from what we're given.
+#
+# 11:15, restate my assumptions:
+# 1. hex digits encode 4 bits (2**4 == 16)
+# 2. base64 digits encode 6 bits (2**6 == 64)
+#
+# So, according to the example, hex "492" == base64 "SS"
 #    4   9   2
 # 010010010010
 #      S     S
-# So 'S' is 0b010010 or 18, implying 'A' is the first digit; assume A..Z are digits 0..25 in b64
+# This implies 'S' is 0b010010 or 18, implying 'A' is the first digit;
+# assume A..Z are digits 0..25 in base64
 # What about the rest?
-# hex 76d == b64 dt
+#
+# Look at an example that has lowercase in the base64: hex "76d" == base64 "dt"
 #    7   6   d
 # 011101101101
 #      d     t
-# d = 29, t = 45
-# So 'd' is 0b011101 or 29 in b64, implying 'a' is 26 and a..z are digits 26..51 in b64
+# So 'd' is 0b011101 or 29 in base64;
+# assume 'a' is 26 and a..z are digits 26..51 in base64
 # Presumably 0-9 are digits 52..62, but let's check:
-# (from the end) hex f6d == b64 9t
+#
+# (from the end) hex "f6d" == base64 "9t"
 #    f   6   d
 # 111101101101
 #      9     t
-# t is 26 + ("t".ord - "a".ord) == 45 in b64, which matches 0b101101
+# t is 26 + ("t".ord - "a".ord) == 45 in base64, which matches 0b101101
 # '9' is 0b111101 or 61, verifying that 0..9 are digits 51..61
 # But, we're still 2 digits short. The example doesn't contain either of them.
+#
+# Research shows the last two digits vary, but the most common seems to be '+', '/'
 def hex_to_base64(hex_string)
   bytes_to_base64(hex_to_bytes(hex_string))
 end
@@ -108,7 +126,7 @@ describe :bytes_to_base64 do
 end
 
 BYTE_RANGE = 0..255
-B64_DIGITS = ("A".."Z").to_a + ("a".."z").to_a + ("0".."9").to_a
+B64_DIGITS = ("A".."Z").to_a + ("a".."z").to_a + ("0".."9").to_a + ['+', '/']
 BITS_PER_B64_DIGIT = 6
 B64_DIGIT_MASK = 2**BITS_PER_B64_DIGIT - 1
 B64_DIGITS_PER_SLICE = 4
