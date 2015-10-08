3e6985fe2d679a77f355244bca4fbcf85cc72f2f
Jon Bauman <jongithub@shumi.fake>
2015-10-07 23:44:39 -0400

    Add more tests and make them pass (and fix standalone execution)

--- fixed_xor.rb
+++ fixed_xor.rb
@@ -11,11 +11,12 @@
 
 # 746865206b696420646f6e277420706c6179
 
+
 if __FILE__ == $0
   puts `rspec -f documentation #{__FILE__}`
-else
-  require "rspec"
 end
+
+require "rspec"
 
 describe :fixed_xor do
   HEX1 = "1c0111001f010100061a024b53535009181c"
@@ -25,7 +26,19 @@ describe :fixed_xor do
   it "returns #{XOR} when called with #{HEX1} and #{HEX2}" do
     expect(fixed_xor(HEX1, HEX2)).to eq(XOR)
   end
+
+  it "gives an error on invalid input" do
+    expect { fixed_xor() }.to raise_error(ArgumentError)
+    expect { fixed_xor("beef", "cake") }.to raise_error(ArgumentError)
+    expect { fixed_xor("ded", "b0d") }.to raise_error(ArgumentError)
+  end
 end
 
-def fixed_xor(*buffers)
+def fixed_xor(*hex_buffers)
+  fail ArgumentError, "buffers must be equal length" if hex_buffers.map(&:length).uniq.length != 1
+  fail ArgumentError, "buffers must be full bytes" if hex_buffers.first.length.odd?
+
+  byte_arrays = hex_buffers.map {|hex| hex.scan(/../).map {|hex_byte| Integer(hex_byte, 16) } }
+  xored_bytes = byte_arrays.reduce {|a, e| a.zip(e).map {|bytes| bytes.reduce(&:^) } }
+  xored_bytes.map(&:chr).join("").unpack("H*").first
 end
