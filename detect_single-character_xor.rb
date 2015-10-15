e412b375ee3966c988d5b606da8cc007a5ae528c
Jon Bauman <jongithub@shumi.fake>
2015-10-14 17:30:08 -0700

    Add downcase_ciphertext and tests

--- detect_single-character_xor.rb
+++ detect_single-character_xor.rb
@@ -12,16 +12,37 @@ end
 require "rspec"
 require "securerandom"
 
+require "./single-byte_xor_cipher"
+
 describe :find_ciphertext do
-  HEX_CIPHERTEXT = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
-  possible_ciphertexts = Array.new(99).map { SecureRandom.hex(HEX_CIPHERTEXT.length) }
-  possible_ciphertexts << HEX_CIPHERTEXT
-  possible_ciphertexts.shuffle!
+  possible_hex_ciphertexts = Array.new(5).map { SecureRandom.hex(HEX_CIPHERTEXT.length) }
+  possible_hex_ciphertexts << HEX_CIPHERTEXT
+  possible_hex_ciphertexts.shuffle!
 
   it "finds the hex string that has been encrypted by single-character XOR" do
-    expect(find_ciphertext(possible_ciphertexts)).to eq(HEX_CIPHERTEXT)
+    expect(find_ciphertext(possible_hex_ciphertexts)).to eq(HEX_CIPHERTEXT)
   end
 end
 
-def find_ciphertext(possible_ciphertexts)
+describe :downcase_ciphertext do
+  it "lowers the case of encrypted text" do
+    expect(decrypt(HEX_CIPHERTEXT, hex_key: KEY)).to eq(raw_to_hex(PLAINTEXT))
+    expect(decrypt(HEX_CIPHERTEXT, hex_key: KEY)).to_not eq(raw_to_hex(PLAINTEXT.downcase))
+
+    downcased_hex_ciphertext = raw_to_hex(downcase_ciphertext(hex_to_raw(HEX_CIPHERTEXT)))
+    expect(decrypt(downcased_hex_ciphertext, hex_key: KEY)).to eq(raw_to_hex(PLAINTEXT.downcase))
+  end
+
+end
+
+def raw_to_hex(raw)
+  raw.unpack("H*").first
+end
+
+def downcase_ciphertext(ciphertext)
+  case_bit = ("A".ord ^ "a".ord)
+  ciphertext.bytes.map {|c| c | case_bit }.map(&:chr).join
+end
+
+def find_ciphertext(possible_hex_ciphertexts)
 end
