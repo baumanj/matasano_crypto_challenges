479af00b36db8bbae77b08b3c6a4cd66833310de
Jon Bauman <jongithub@shumi.fake>
2015-10-20 01:34:42 -0700

    Add failing test for set 2 challenge 11 (An ECB/CBC detection oracle)

---
+++ an_ecb-cbc_detection_oracle.rb
@@ -0,0 +1,58 @@
+# An ECB/CBC detection oracle
+# Now that you have ECB and CBC working:
+
+# Write a function to generate a random AES key; that's just 16 random bytes.
+
+# Write a function that encrypts data under an unknown key --- that is, a function that generates a random key and encrypts under it.
+
+# The function should look like:
+
+# encryption_oracle(your-input)
+# => [MEANINGLESS JIBBER JABBER]
+# Under the hood, have the function append 5-10 bytes (count chosen randomly) before the plaintext and 5-10 bytes after the plaintext.
+
+# Now, have the function choose to encrypt under ECB 1/2 the time, and under CBC the other half (just use random IVs each time for CBC). Use rand(2) to decide which to use.
+
+# Detect the block cipher mode the function is using each time. You should end up with a piece of code that, pointed at a block box that might be encrypting ECB or CBC, tells you which one is happening.
+
+if __FILE__ == $0
+  puts `rspec -f documentation #{__FILE__}`
+end
+
+require "rspec"
+require "securerandom"
+
+require "./crypto"
+
+describe :encryption_oracle do
+  it "determines whether ECB or CBC is being used 100 times in 100" do
+    actual_mode = nil
+    100.times do
+      guessed_mode = send(subject) do |plaintext|
+        actual_mode, ciphertext = random_encrypt(plaintext)
+        ciphertext
+      end
+      expect(guessed_mode).to eq(actual_mode)
+    end
+  end
+end
+
+def random_encrypt(plaintext)
+  key = SecureRandom.random_bytes(16)
+  added_bytes = "X" * (5 + rand(6))
+  plaintext = "#{added_bytes}#{plaintext}#{added_bytes}"
+  if rand(2).zero?
+    print "ECB(#{plaintext}) =>\t"
+    [:ECB, encrypt_aes_128_ecb(plaintext, key)]
+  else
+    print "CBC(#{plaintext}) =>\t"
+    iv = SecureRandom.random_bytes(16)
+    [:CBC, encrypt_aes_128_cbc(plaintext, iv, key)]
+  end
+end
+
+def encryption_oracle
+  block_size = 16
+  ciphertext = yield "YELLOW SUBMARINE" * 3 # probably anything 3x block size
+  :ECB
+end
